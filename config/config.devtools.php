82981421316e7e0e99c479733f7058962e4fa284
Dmitry Korolev <chameleonweb2012@gmail.com>
2017-09-09 04:40:29 +0300

    add to config devtols APP_ENV

--- config/config.devtools.php
+++ config/config.devtools.php
@@ -1,6 +1,7 @@
 <?php
 
 define('BASE_PATH', dirname(__DIR__));
+define('APP_ENV', getenv('APP_ENV') ?: 'development');
 
 include BASE_PATH . '/config/loader.php';
 include BASE_PATH . '/config/config.php';
